f2e806862600cf11866e6dc5f5819d177bd8d7bd
Leandro Tenorio da Silva <tenorioleandro22@gmail.com>
2025-10-14 16:15:31 -0600

    chore: adicionar meta mobile-web-app-capable

--- app/layout.jsx
+++ app/layout.jsx
@@ -48,6 +48,7 @@ export default function RootLayout({ children }) {
 
         {/* iOS PWA */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <link rel="icon" href="/favicon.ico" />
